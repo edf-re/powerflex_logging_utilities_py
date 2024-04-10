cf0b7c9c7c249a1d5226804019f642a345e80ea0
Hugo O. Rivera (ROGUH) <hugo.rivera@powerflex.com>
2024-04-10 11:12:49 -0600

    Change dep python-json-logger>=2.0.4 to >=2.0.0
    Less strict

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     python_requires=">2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, !=3.7.*",
     install_requires=[
         "aiodebug>=2.3.0",
-        "python-json-logger>=2.0.4",
+        "python-json-logger>=2.0.0",
     ],
     extras_require={
         "pydantic": ["pydantic>=1.9"],
